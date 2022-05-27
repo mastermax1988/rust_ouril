a71a9a7e4aac74564143e03582475bd16ef3ba8b
Max Huber <huber.maxi@gmail.com>
2022-05-27 10:50:11 +0200

    turn & score

--- src/board.rs
+++ src/board.rs
@@ -1,13 +1,16 @@
+#[derive(Copy, Clone)]
 pub struct Board {
     field: [u8; 12],
-    current_player: u8,
+    current_player: usize,
+    score: [u8; 2],
 }
 
 impl Board {
     pub fn new() -> Board {
         Board {
             field: [4; 12],
-            current_player: 1,
+            current_player: 0,
+            score: [0; 2],
         }
     }
 
@@ -16,7 +19,7 @@ impl Board {
             return false;
         }
         let mut index = match self.current_player {
-            1 => start,
+            0 => start,
             _ => start + 6,
         };
 
@@ -36,14 +39,54 @@ impl Board {
             self.field[index] += 1;
             count -= 1;
         }
+        self.score(index);
 
-        self.current_player += 1;
-        self.current_player %= 2;
+        self.set_next_player();
 
         true
     }
 
+    fn set_next_player(&mut self) {
+        self.current_player += 1;
+        self.current_player %= 2;
+    }
+
+    fn score(&mut self, end: usize) {
+        if (self.current_player == 0 && end < 6) || (self.current_player == 1 && end > 5) {
+            return;
+        }
+        let backup_field = self.field.clone();
+        let backup_score = self.score.clone();
+        let min = match self.current_player {
+            0 => 6,
+            _ => 0,
+        };
+        let mut index = end;
+        while (self.field[index] == 2 || self.field[index] == 3) && index >= min {
+            self.score[self.current_player] += self.field[index];
+            self.field[index] = 0;
+            if index > 0 {
+                index -= 1;
+            } else {
+                break;
+            }
+        }
+        let start = match self.current_player {
+            0 => 6,
+            _ => 0,
+        };
+        for i in 0..6 {
+            if self.field[i + start] > 0 {
+                return;
+            }
+        }
+        println!("gran slam!");
+        self.score = backup_score;
+        self.field = backup_field;
+    }
+
     pub fn print(&self) {
+        println!("{} : {}", self.score[0], self.score[1]);
         for i in 0..6 {
             print!("{} | ", self.field[11 - i]);
         }
@@ -52,6 +95,7 @@ impl Board {
             print!("{} | ", self.field[i]);
         }
         println!();
+        println!("Next player: {}", self.current_player);
         println!()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
+extern crate core;
 mod board;
+
 use crate::board::Board;
+
+use text_io::read;
 
 fn main() {
     let mut b = Board::new();
     b.print();
-    b.turn(5);
-    b.print();
-    b.turn(3);
-    b.print();
+    while true {
+        let line: String = read!("{}\n");
+        let nr: usize;
+        if let Ok(x) = line.parse() {
+            nr = x
+        } else {
+            continue;
+        };
+        if nr < 6 {
+            b.turn(nr);
+            b.print();
+        }
+    }
 }
